4b1b70779c778f119da77a803c6b09805a929d52
Ady Castueras <ady.castueras@gmail.com>
2023-04-13 14:28:30 +0800

    Displays posts on page

--- app/javascript/react/index.js
+++ app/javascript/react/index.js
@@ -1,5 +1,9 @@
 import { define } from 'remount'
 
 import MyComponent from './MyComponent'
+import PostsDashboard from './PostsDashboard'
 
-define({'x-my-component': MyComponent})
+define({
+  'x-my-component': MyComponent,
+  'x-posts-dashboard': PostsDashboard
+})
